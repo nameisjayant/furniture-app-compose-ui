407311e41328cf5d15e2460cc4fd690450ea8e38
nameisjayant <jayntkumar99@gmail.com>
2023-02-03 11:43:41 +0530

    product details screen

--- app/src/main/java/com/nameisjayant/furnitureapp/commonUi/CommonUi.kt
+++ app/src/main/java/com/nameisjayant/furnitureapp/commonUi/CommonUi.kt
@@ -1,10 +1,17 @@
 package com.nameisjayant.furnitureapp.commonUi
 
+import android.annotation.SuppressLint
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -17,4 +24,16 @@ fun SpacerHeight(height: Dp = 5.dp) {
 @Composable
 fun SpacerWidth(width: Dp = 5.dp) {
     Spacer(modifier = Modifier.width(width))
+}
+@SuppressLint("ModifierParameter")
+@Composable
+fun AppIconButton(
+    @DrawableRes icon:Int,
+    tint:Color = Color.Unspecified,
+    modifier: Modifier = Modifier,
+    onClick:()->Unit ={}
+) {
+    IconButton(onClick = {onClick()}, modifier = modifier.size(32.dp)) {
+       Icon(painter = painterResource(id = icon), contentDescription = "", tint = tint)
+    }
 }
--- app/src/main/java/com/nameisjayant/furnitureapp/screens/ProductDetailScreen.kt
+++ app/src/main/java/com/nameisjayant/furnitureapp/screens/ProductDetailScreen.kt
@@ -1,23 +1,33 @@
 package com.nameisjayant.furnitureapp.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Minimize
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nameisjayant.furnitureapp.R
+import com.nameisjayant.furnitureapp.commonUi.AppIconButton
+import com.nameisjayant.furnitureapp.commonUi.SpacerHeight
+import com.nameisjayant.furnitureapp.ui.theme.DarkOrange
 import com.nameisjayant.furnitureapp.ui.theme.LightGray
 import com.nameisjayant.furnitureapp.ui.theme.PoppinFont
 import com.nameisjayant.furnitureapp.ui.theme.TextColor
@@ -38,17 +48,100 @@ fun ProductDetailScreen() {
                     .height(250.dp)
                     .fillMaxWidth()
             )
-              Card(
-                  modifier = Modifier
-                      .padding(top = 230.dp)
-                      .fillMaxWidth(),
-                  shape = RoundedCornerShape(20.dp),
-                  elevation = 0.dp
-              ) {
-                  Text(text = "djhjjjff\nfjn\nfnjf", modifier = Modifier.padding(20.dp))
-              }
+            HeaderIcon()
+            Card(
+                modifier = Modifier
+                    .padding(top = 230.dp)
+                    .fillMaxSize(),
+                shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+            ) {
+                LazyColumn {
+                    item {
+                        ProductHeaderSection()
+                    }
+                }
+            }
 
         }
     }
 
 }
+
+@Composable
+fun ProductHeaderSection() {
+    var count by remember { mutableStateOf("0") }
+    Column(
+        modifier = Modifier.padding(20.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.product_name), style = TextStyle(
+                color = TextColor,
+                fontWeight = FontWeight.W400,
+                fontSize = 20.sp,
+            )
+        )
+        SpacerHeight(10.dp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "$599", style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.W600,
+                    color = DarkOrange
+                )
+            )
+            Row(
+            ) {
+                ProductCount(icon = Icons.Default.Minimize){
+                    if(count != "0")
+                    count=count.toInt().minus(1).toString()
+                }
+                Text(text = count, style = TextStyle(
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.W400,
+                    color = TextColor,
+                ),
+                modifier = Modifier.align(CenterVertically).padding(horizontal = 15.dp)
+                )
+                ProductCount(icon = Icons.Default.Add){
+                    count = count.toInt().plus(1).toString()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductCount(
+    icon: ImageVector,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    TextButton(
+        onClick = { onClick() }, modifier = modifier.size(24.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = ButtonDefaults.elevation(0.dp),
+        border = BorderStroke(2.dp, DarkOrange)
+    ) {
+        Icon(icon, contentDescription = "", tint = DarkOrange)
+    }
+}
+
+@Composable
+fun HeaderIcon() {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 15.dp, vertical = 50.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        AppIconButton(icon = R.drawable.back)
+        AppIconButton(icon = R.drawable.share)
+    }
+
+}
+
+
